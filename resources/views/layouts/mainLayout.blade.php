e6eacfe4a6255417f063ab62ef793cba98974f15
Luis Brito <britoluisea@gmail.com>
2022-09-08 13:58:58 -0500

    instalando dependencias, plugins, creando layout

---
+++ resources/views/layouts/mainLayout.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    <link rel="stylesheet" href="{{ URL::asset('scss/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <script src="{{ URL::asset('js/app.js') }}" defer></script>
+    <script src="{{ URL::asset('js/main.js') }}" defer></script>
+</head>
+<body>
+    <div id="app">
+    </div>
+</body>
+</html>
